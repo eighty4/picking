87cd42c9f14150ffa235c5e040d2d923a73b75ce
Adam McKee <adam.be.g84d@gmail.com>
2023-09-07 22:25:27 -0500

    specify default text style in screen widget

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'theme.dart';
 
@@ -11,6 +12,11 @@ class PickingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = PickingTheme.of(context);
     return Scaffold(
-        backgroundColor: theme.backgroundColor, body: SafeArea(child: child));
+        backgroundColor: theme.backgroundColor,
+        body: SafeArea(
+            child: DefaultTextStyle(
+                style: GoogleFonts.cormorant()
+                    .copyWith(color: theme.textColor, letterSpacing: 1.25),
+                child: child)));
   }
 }
